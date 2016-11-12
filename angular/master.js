f88be6b92cb3b04a0eb1e35b39bfa7a4b23b9dc7
Alex W <wapniarski@gmail.com>
2016-11-11 17:27:57 -0800

    updated skills and portfolio

--- angular/master.js
+++ angular/master.js
@@ -31,7 +31,7 @@ app.factory('MainFactory', function() {
     var factory = {};
 
     factory.languages = ["JavaScript","Python","Swift"];
-    factory.frameworks = ["Node.js","AngularJS","Express.js","jQuery","Flask","React.js"];
+    factory.frameworks = ["Node.js","AngularJS","Express.js","jQuery","Flask"]; // ,"React.js"
     factory.databases = ["MySQL", "MongoDB", "SQL", "NoSQL"];  
     factory.tools = ["Git", "AWS EC2","AJAX","Unix/Linux","Socket.io","Xcode"];
     factory.design = ["HTML5","CSS3","Bootstrap","Materialize","Photoshop","Illustrator"];
